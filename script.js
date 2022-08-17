7f4e98ab200ddb8abb9ece6d67fb0120eed56efc
2022sunil <109381600+2022sunil@users.noreply.github.com>
2022-08-17 13:33:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// navbar variables
+const navbarNav = document.querySelector('.navbar-nav');
+const navbarToggleBtn = document.querySelector('.nav-toggle-btn');
+
+// navbar toggle functionality
+navbarToggleBtn.addEventListener('click', function () {
+
+  navbarNav.classList.toggle('active');
+  this.classList.toggle('active');
+
+});
+
+
+let calculateBtn = document.getElementById("calculate-btn");
+let result = document.getElementById("result");
+let calculate = () => {
+  let p = Number(document.getElementById("principal").value);
+  let r = Number(document.getElementById("rate").value);
+  let t = Number(document.getElementById("time").value);
+  let duration = document.getElementById("duration").value;
+  let simpleInterest =
+    duration == "year" ? (p * r * t) / 100 : (p * r * t) / 1200;
+  let amount = p + simpleInterest;
+  result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
+  <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
+  <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
+};
+calculateBtn.addEventListener("click", calculate);
+window.addEventListener("load", calculate);
